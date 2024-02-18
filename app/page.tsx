b5ff575833b62658df9797e6499c388c82759745
Asro <asro@citis.kompasgramedia.com>
2024-02-18 21:22:47 +0700

    fix: remove async in default function

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ import { fetchRevenue } from '@/app/lib/data';
 
 
 export default function Page() {
-  const revenue = await fetchRevenue();
   return (
     <main className="flex min-h-screen flex-col p-6">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
