03ef9e3c74ab77e1b20c496ba54825a086af4de5
Asro <asro@citis.kompasgramedia.com>
2024-04-12 02:50:14 +0700

    fix: add speed insight within layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
